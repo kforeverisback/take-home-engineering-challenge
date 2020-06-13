077e1e347b24521ae8c1b01eb7097fee4542d0ee
Kushal (Mohammad) <mohammad.azim.ul.ekram@intel.com>
2020-06-12 20:51:01 -0700

    Added custom output formatting(json, csv, console). Added 3rd party distance calculation code, in case we want to run offline

--- sffdtruck/__main__.py
+++ sffdtruck/__main__.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 import argparse
 import csv
+import json
 import os
 import pickle
 from io import StringIO
 
-from sffdtruck import CONSTS, __version__, dist_haversine, kdtree
-from sffdtruck.retrieve import retrieve, update_cache
-from vincenty import vincenty as dist_vincenty
+from sffdtruck import CONSTS, __version__, kdtree
+from sffdtruck.retrieve import retrieve
+
+# Importing 3rd party distance calculation packages
+# If not downloaded, we'll use local version.
+# We've added a local version to make
+try:
+    from vincenty import vincenty as dist_vincenty
+except ImportError:
+    from sffdtruck.external.vincenty import vincenty as dist_vincenty
+
+try:
+    from haversine import haversine as dist_haversine
+except ImportError:
+    from sffdtruck.external.haversine import haversine as dist_haversine
 
 
 def arg_parser():
@@ -31,12 +44,7 @@ def arg_parser():
         "--timeout", "-t", default=3, type=int, help="""Download timeout."""
     )
     parser.add_argument(
-        "--count",
-        "-c",
-        default=5,
-        type=int,
-        choices=range(1, 100),
-        help="""Number of food truck locations to return.""",
+        "--count", "-c", default=5, type=int, help="""Number of locations to return.""",
     )
     parser.add_argument(
         "--dist",
@@ -45,23 +53,95 @@ def arg_parser():
         choices=["euclidean", "e", "haversine", "h", "vincenty", "v"],
         help="""Distance calculation function""",
     )
+    parser.add_argument(
+        "--format",
+        "-f",
+        default="plain",
+        choices=["csv", "json", "plain"],
+        help="""Output format (in closest first order)""",
+    )
     parser.add_argument("--version", "-v", action="version", version=version)
     return parser
+
+
+def get_dist_fn(dist_type):
+    if dist_type[0] == "h":
+        dist_fn = dist_haversine
+    elif dist_type[0] == "v":
+        dist_fn = dist_vincenty
+    else:
+        dist_fn = kdtree.dist_sq
+    return dist_fn
+
+
+def get_output(out_type, input_nodes):
+    if input_nodes is None or len(input_nodes) == 0:
+        return "Nothing was found in the search!!"
+
+    capture = ["Applicant", "Address", "FacilityType", "FoodItems"]
+    console_format = [
+        "Food Truck Name : {}",
+        "Address         : {}",
+        "Truck Type      : {}",
+        "Available Items : {}",
+        "Location        : {}",
+    ]
+    out = None
+    if out_type[0] == "c":
+        header = ",".join(capture) + ",Location\n"
+        with StringIO() as sio:
+            sio.writelines(header)
+            for inode in input_nodes:
+                sio.writelines(
+                    [
+                        ",".join([inode.ft_data.data[c] for c in capture]),
+                        f',{inode.ft_data.data["Location"]}',
+                        "\n",
+                    ]
+                )
+            out = sio.getvalue()
+    elif out_type[0] == "j":
+        out = {
+            "food_trucks": [
+                {
+                    **{c: inode.ft_data.data[c] for c in capture},
+                    **{"Location": inode.ft_data.latlon},
+                }
+                for inode in input_nodes
+            ]
+        }
+        out = json.dumps(out, indent=2)
+    else:
+        with StringIO() as sio:
+            sio.writelines("----(Closest)---- \n")
+            for inode in input_nodes:
+                form_val = [inode.ft_data.data[c] for c in capture]
+                form_val.append(inode.ft_data.latlon)
+                sio.writelines("\n".join(console_format).format(*form_val))
+                sio.writelines("\n-----------------\n")
+            out = sio.getvalue()
+
+    return out
 
 
 def main(args=None):
     args = arg_parser().parse_args(args)
 
-    csv_bytes, is_newer = retrieve(args.update)
+    csv_bytes, is_newer = retrieve(check_update=args.update, timeout=args.timeout)
     if is_newer:
         # We read CSV and 're-'build KDTree cache
         with StringIO(csv_bytes) as sio:
-            entries = [
+            all_csv_entries = list(csv.DictReader(sio))
+            valid_entries = [
                 kdtree.FTData(d)
-                for d in list(csv.DictReader(sio))
+                for d in all_csv_entries
                 if float(d["Latitude"]) != 0 and float(d["Longitude"]) != 0
             ]
-            tree_root = kdtree.FTNode.build_tree(entries, dist_fn=dist_haversine)
+            if len(all_csv_entries) != len(valid_entries):
+                print(
+                    f"There were {len(all_csv_entries) - len(valid_entries)} entries with no Lat/Lon values"
+                )
+            tree_root = kdtree.FTNode.build_tree(valid_entries)
             with open(CONSTS.kdtree_cache_file, "wb") as f:
                 # Pickle the 'data' dictionary using the highest protocol available.
                 pickle.dump(tree_root, f, pickle.HIGHEST_PROTOCOL)
@@ -73,14 +153,11 @@ def main(args=None):
     # Now we have a tree_root
     # Perform the search
     lat_lon = (float(args.lat_lon[0]), float(args.lat_lon[1]))
-    res = tree_root.search(lat_lon, k=args.count)
-    [print(r) for r in res]
-    if args.dist[0] == "h":
-        pass
-    elif args.dist[0] == "v":
-        pass
-    else:
-        pass
+    res = tree_root.search(lat_lon, k=args.count, dist_fn=get_dist_fn(args.dist[0]))
+
+    # s=json.dumps({'output':[r.ft_data.to_json() for r in res]}, indent=2)
+    # for r in res:
+    print(get_output(args.format, res))
 
 
 if __name__ == "__main__":
